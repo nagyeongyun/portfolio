6bea79839c2954969e0fb6f5a37cd8bc687f9237
[AI10] 나경윤 <skruddbs99@naver.com>
2024-10-04 07:03:58 +0900

    feat: 이미지 모달에 로딩 아이콘 추가

--- src/components/common/ImgModal.tsx
+++ src/components/common/ImgModal.tsx
@@ -1,22 +1,47 @@
+import { useState, useEffect } from 'react';
 import Portal from './Portal';
-import useModal from 'hooks/useModal';
 import { ReactComponent as DeleteBtn } from '../../assets/icon/delete-btn.svg';
 import { ImgModalProps } from 'types/Modal';
+import { LoadingIcon } from './LoadingIcon';
 
 const ImgModal = ({ img, toggleModal, modalRef }: ImgModalProps) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = img;
+
+    imageLoader.onload = () => {
+      setLoading(false);
+    };
+
+    return () => {
+      imageLoader.onload = null;
+    };
+  }, [img]);
+
   return (
     <Portal>
       <div className="fixed inset-0 flex items-center justify-center bg-zinc-800 bg-opacity-70">
         <div className="relative" ref={modalRef}>
-          <img
-            src={img}
-            alt="img modal"
-            className="max-w-full max-h-full border border-slate-200"
-          />
-          <DeleteBtn
-            onClick={toggleModal}
-            className="absolute top-2 right-2 cursor-pointer hover:opacity-80"
-          />
+          {loading ? (
+            <div className="p-28">
+              <LoadingIcon />
+            </div>
+          ) : (
+            <>
+              <img
+                src={img}
+                alt="img modal"
+                className="w-full max-w-[70vw] max-h-[60vh] md:max-w-full md:max-h-full border border-slate-200"
+                onLoad={() => setLoading(false)}
+              />
+              <DeleteBtn
+                onClick={toggleModal}
+                className="absolute top-2 right-2 cursor-pointer hover:opacity-80"
+              />
+            </>
+          )}
         </div>
       </div>
     </Portal>
